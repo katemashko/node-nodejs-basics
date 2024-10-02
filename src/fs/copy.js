7fe9a5826aea9d9091df7d6f1b12c6faedf8d782
Katsiaryna Mashko <katemashko@gmail.com>
2024-10-02 22:28:32 +0300

    feat: implement copy file

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+import fs from "node:fs/promises";
+
+const folderExists = async (folderPath) => {
+  try {
+    await fs.access(folderPath, fs.constants.F_OK);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
 const copy = async () => {
-    // Write your code here 
+  if (
+    !(await folderExists("src/fs/files")) ||
+    (await folderExists("src/fs/files_copy"))
+  ) {
+    throw new Error("FS operation failed");
+  }
+  await fs.cp(
+    "src/fs/files",
+    "src/fs/files_copy",
+    { recursive: true },
+    { errorOnExist: true },
+    (err) => {
+      console.log(err);
+    }
+  );
 };
 
 await copy();
