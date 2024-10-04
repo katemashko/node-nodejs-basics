e40f98a124879122f9f76d59e22813b114847cd7
Katsiaryna Mashko <katemashko@gmail.com>
2024-10-04 13:45:11 +0300

    feat: implement args file

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const argsOutputArray = [];
+
+  for (let index = 0; index < args.length; index += 2) {
+    const key = args[index].replace("--", "");
+    const value = args[index + 1];
+    argsOutputArray.push(`${key} is ${value}`);
+  }
+  console.log(argsOutputArray.join(", "));
 };
 
 parseArgs();
