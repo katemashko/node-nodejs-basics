2da98ac848e7c133061ebf73a0f0d8d7b56948a3
Katsiaryna Mashko <katemashko@gmail.com>
2024-10-07 21:35:57 +0300

    refactor: refactor read file

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,11 +1,12 @@
 import fs from "node:fs";
+import os from "node:os";
 
 const filePath = "src/streams/files/fileToRead.txt";
 const readFileStream = fs.createReadStream(filePath);
 
 const read = async () => {
   await readFileStream.on("data", (chunk) => {
-    process.stdout.write(chunk.toString());
+    process.stdout.write(chunk.toString() + os.EOL);
   });
 };
 
