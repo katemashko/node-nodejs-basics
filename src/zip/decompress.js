4c5d2cfe4f2122b912a94019209179b04642d790
Katsiaryna Mashko <katemashko@gmail.com>
2024-10-07 22:19:10 +0300

    feat: implement decompress file

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import fs from "node:fs";
+import zlib from "node:zlib";
+
+const inputFileArchive = "src/zip/files/archive.gz";
+const outputFilePath = "src/zip/files/fileToCompress.txt";
+
 const decompress = async () => {
-    // Write your code here 
+  const gunzip = zlib.createGunzip();
+  const sourceFile = await fs.createReadStream(inputFileArchive);
+  const destinationFile = await fs.createWriteStream(outputFilePath);
+
+  sourceFile.pipe(gunzip).pipe(destinationFile);
 };
 
 await decompress();
